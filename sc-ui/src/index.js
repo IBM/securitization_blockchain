f99effdd777f6b863499eabb0be3393aedb011ba
Kalonji Bankole <kkbankol@us.ibm.com>
2018-06-18 11:40:38 -0700

    updating readme, cf manifest

--- sc-ui/src/index.js
+++ sc-ui/src/index.js
@@ -6,3 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// const port = process.env.PORT || 4000;
+// app.listen(port);
